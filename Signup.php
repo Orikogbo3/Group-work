1aaf0743498c68ce4788e828592185d8d6555550
Orikogbo3 <157710249+Orikogbo3@users.noreply.github.com>
2024-04-16 07:01:10 +0100

    Updating file
    uploading the updated file to send for combine work

--- Signup.php
+++ Signup.php
@@ -1,43 +1,38 @@
 <?php
-    session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database1";
 
-    include("db.php");
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($_SERVER['REQUEST_METHOD'] == "POST") 
-    {
-        // Retrieve form data
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $number= $_POST['number'];
-        $password = $_POST['password'];
-        $cpassword = $_POST['cpassword'];
-        
-
-        if(!empty($email) && !empty($password) && !empty($cpassword) && !is_numeric($email))
-        {
-
-            $query = "insert into form (username, email, number, password, cpassword) values('$username', '$email', '$number', '$password', '$cpassword')";
-
-            $result = mysqli_query($con, $query);
-
-            if($result) 
-            {
-                echo "<script>alert('Successfully Registered');</script>";
-            }
-            else {
-                echo "<script>alert('Errror: " . mysqli_error($con) . "');</script>";
-            }
-
-        } else   { 
-
-            echo "<script> alert('Please Enter some valid Information');</script>";
-
-        }
-        
-    }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
+<?php
+if(isset($_POST['register'])){
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $phone= $_POST['phone'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    
+    if($password !== $confirm_password){
+        echo "Passwords do not match!";
+    } else {
+        
+        $sql = "INSERT INTO login1 (fullname, email, phone, password) VALUES ('$fullname', '$email', '$phone', '$password')";
 
-<!DOCTYPE html>
+        if ($conn->query($sql) === TRUE) {
+            echo "Registration successful!";
+            header("location:login1.php");
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -51,18 +46,18 @@
             <a href=""><img src="unerground aberdeen.png" width="200" class="logo"></a>
             <ion-icon name="call-outline"></ion-icon>
             <ul>
-                <li><a href="Homepage.php">Home</a></li>
+                <li><a href="Homepage.html">Home</a></li>
                  <li><a href="#">Help & Support</a>    <ion-icon name="call-outline" style= "font-size: 20px; color: #fff;"  ></ion-icon></li>
                 <li><a href="about.html">About</a></li>
             </ul>
         </div>
 
 <div class="container">
-    <form action="#" method="POST">
+    <form action="" method="post">
         <h2>Sign Up</h2>
         <div class="form-group">
-            <label for="name">Full name</label>
-            <input type="text" id="username" name="username" placeholder="Full Name" required>
+            <label for="text">Full name</label>
+            <input type="text" id="name" name="fullname" placeholder="Full Name" required>
         </div>
         <div class="form-group">
             <label for="email">Email</label>
@@ -71,7 +66,7 @@
         </div>
          <div class="form-group">
             <label for="mobile">Phone Number</label>
-            <input type="text" id="number" name="number" placeholder="Your phone number" required>
+            <input type="text" id="phone number" name="phone" placeholder="Your phone number" required>
         </div>
         <div class="form-group">
             <label for="password">Password</label>
@@ -80,11 +75,11 @@
        
         <div class="form-group">
             <label for="confirm-password">Confirm Password</label>
-            <input type="password" id="cpassword" name="cpassword" placeholder="Confirm your password" required>
+            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm your password" required>
         </div>
         <p><input type="checkbox">Agree to the terms & conditions</p>
-        <button type="submit">Sign Up</button>
-        <p>Already have an account? <a href="loginabb.php">Log In</a></p>
+        <button type="submit" name=register>Sign in</button>
+        <p>Already have an account? <a href="login1.php">Log In</a></p>
     </form>
 </div>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
